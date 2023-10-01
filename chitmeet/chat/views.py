b524b59e9814d70942ec8d18c23bf6998069afd9
Sambit <sambit@Sambits-MacBook-Pro.local>
2023-10-01 17:34:07 +0530

    created views for chat app

--- chitmeet/chat/views.py
+++ chitmeet/chat/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request, *args, **kwargs):
+    print("args: ", args)
+    print("kwargs: ", kwargs)
+    return render(request, "chat/index.html")
