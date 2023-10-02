06abae23f71fb6af3c4045ccd8f9b2b34e535368
Sambit Kumar <sambit.kumar142@gmail.com>
2023-10-02 19:33:31 +0530

    updated settings to use redis channel layer

--- chitmeet/chitmeet/settings.py
+++ chitmeet/chitmeet/settings.py
@@ -127,6 +127,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+CHANNEL_LAYERS = {
+    "default" : {
+        "BACKEND" : "channels_redis.core.RedisChannelLayer",
+        "CONFIG" : {
+            "hosts" : [ ("localhost", 6379) ],
+        }
+    }
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
